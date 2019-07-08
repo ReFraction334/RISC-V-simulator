53a0f0403466f773b5557a5848fc10ffac2075b1
Yi Wang <44198739+ReFraction334@users.noreply.github.com>
2019-07-08 15:28:37 +0800

    without 5-stage pipeline

---
+++ cmake-build-debug/CMakeFiles/RISC_V.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RISC_V.dir/code.cpp.o"
+  "RISC_V.pdb"
+  "RISC_V"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RISC_V.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
